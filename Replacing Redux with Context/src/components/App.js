836b82cdf7217f9f71ad056bfe0b24d2d568ca52
Ivan Velkov <ivanv3lkov@gmail.com>
2021-01-18 13:11:17 +0200

    Replacing React with Context - simple example

---
+++ Replacing Redux with Context/src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import { LanguageStore } from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="ui container">
+        <LanguageStore>
+          <LanguageSelector />
+
+          <ColorContext.Provider value="red">
+            <UserCreate />
+          </ColorContext.Provider>
+        </LanguageStore>
+      </div>
+    );
+  }
+}
+
+export default App;
